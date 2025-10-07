39ccc48763905aacaf0b9792410467526b0e2514
anthanh <thanhan.lan@gmail.com>
2025-10-07 23:31:11 +0700

    update navigation

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -3,9 +3,36 @@
 </style>
 
 <template>
-    <div class="mainHeader">
-        <header>
-            <nav class="flex container items-center">Nav bar</nav>
+    <div>
+        <header class="mainHeader">
+            <div class="bg-slate-500 min-h-16">
+                <div class="w-full px-4">
+                    <div class="flex items-center justify-between flex-nowrap">
+                        <div class="header-wrap-logo">icon</div>
+                        <!-- <div class="header-wrap-menu hidden lg:block">icon</div> -->
+                        <div class="header-wrap-menu flex-1 flex justify-center">
+                            <nav class="navbar-mainmenu">
+                                <ul class="menuList-primary flex flex-row items-center gap-6">
+                                    <li class="has-submenu">
+                                        <span>VỀ BITI'S</span>
+                                    </li>
+                                    <li class="has-submenu">
+                                        <span>NHÃN HIỆU</span>
+                                        <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                                    </li>
+                                    <li class="has-submenu"></li>
+                                    <li class="has-submenu"></li>
+                                    <li class="has-submenu"></li>
+                                    <li class="has-submenu"></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="header-wrap-action ">icon</div>
+                    </div>
+                </div>
+            </div>
         </header>
     </div>
 </template>
+
+<style></style>
